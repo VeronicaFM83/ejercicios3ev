6b010c651240a94af9864e9bb452aa5b00da57f1
VeronicaFM83 <73966112+VeronicaFM83@users.noreply.github.com>
2021-06-16 00:33:02 +0200

    Add files via upload

---
+++ SolucionesExam3Eval/ejercicio3/Ejercicio3.java
@@ -0,0 +1,33 @@
+package fp.daw.exprog20210602.ejercicio3;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Ejercicio3 {
+
+	public static void invertir(int n, Queue<Integer> q) {
+		if (n <= 0)
+			return;
+		if (q == null || n > q.size())
+			throw new IllegalArgumentException();
+		Deque<Integer> p = new ArrayDeque<>();
+		int r = q.size() - n;
+		while (n-- > 0)
+			p.push(q.poll());
+		while (!p.isEmpty())
+			q.offer(p.pop());
+		while (r-- > 0)
+			q.offer(q.poll());
+	}
+	
+	public static void main(String[] args) {
+		Queue<Integer> q = new LinkedList<Integer>(Arrays.asList(2, 13, -42, 21, 4, 9, 14, -3, 11, 5));
+		System.out.println(q);
+		invertir(4, q);
+		System.out.println(q);
+	}
+
+}
